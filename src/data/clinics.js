4670c516a090a2336e23badefc443a73fd4daabe
ibruse <81096630+ibruse@users.noreply.github.com>
2026-06-04 08:55:36 +0530

    Create clinics.js
    Add src/data/clinics.js

---
+++ src/data/clinics.js
@@ -0,0 +1,74 @@
+const clinics = [
+  {
+    id: 1,
+    name: "Smile Dental Clinic",
+    address: "MG Road, Trivandrum",
+    phone: "0471-1234567",
+    vacancy: "Yes"
+  },
+  {
+    id: 2,
+    name: "Trivandrum Dental Centre",
+    address: "Kowdiar, Trivandrum",
+    phone: "0471-2345678",
+    vacancy: "No"
+  },
+  {
+    id: 3,
+    name: "Care Dental Hospital",
+    address: "Pattom, Trivandrum",
+    phone: "0471-3456789",
+    vacancy: "Yes"
+  },
+  {
+    id: 4,
+    name: "Family Dental Care",
+    address: "Vazhuthacaud, Trivandrum",
+    phone: "0471-4567890",
+    vacancy: "No"
+  },
+  {
+    id: 5,
+    name: "Kerala Dental Clinic",
+    address: "Thirumala, Trivandrum",
+    phone: "0471-5678901",
+    vacancy: "Yes"
+  },
+  {
+    id: 6,
+    name: "Elite Dental Studio",
+    address: "Kesavadasapuram, Trivandrum",
+    phone: "0471-6789012",
+    vacancy: "No"
+  },
+  {
+    id: 7,
+    name: "Tooth Care Clinic",
+    address: "Ulloor, Trivandrum",
+    phone: "0471-7890123",
+    vacancy: "Yes"
+  },
+  {
+    id: 8,
+    name: "Dental World",
+    address: "Palayam, Trivandrum",
+    phone: "0471-8901234",
+    vacancy: "No"
+  },
+  {
+    id: 9,
+    name: "New Smile Dental",
+    address: "Sreekaryam, Trivandrum",
+    phone: "0471-9012345",
+    vacancy: "Yes"
+  },
+  {
+    id: 10,
+    name: "PMS Dental Clinic",
+    address: "Kazhakoottam, Trivandrum",
+    phone: "0471-0123456",
+    vacancy: "Yes"
+  }
+];
+
+export default clinics;
